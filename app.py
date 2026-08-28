9a3c7c8e32dca47ffb1f67c9fb603c78f46995d0
Vijay-Nagadamudi <vijaynagadamudi525@gmail.com>
2026-08-28 18:11:13 +0530

    Hand_LandMarker Model Used

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
 import cv2
+import mediapipe as mp 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import HandLandmarksConnections
+
+
 
 from camera.camera import Camera
+from detection.hand_detector import HandDetector
 
 def main():
     camera = Camera()
+    detector = HandDetector()
     
     while True:
         frame = camera.read()
+        results = detector.detect(frame)
+        
+        if results.hand_landmarks:
+            for hand_landmarks in results.hand_landmarks:
+                h, w, _ = frame.shape
+
+                # Draw landmarks
+                for landmark in hand_landmarks:
+                    x = int(landmark.x * w)
+                    y = int(landmark.y * h)
+
+                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+                # Draw connections
+                for connection in HandLandmarksConnections.HAND_CONNECTIONS:
+                    start = hand_landmarks[connection.start]
+                    end = hand_landmarks[connection.end]
+
+                    start_point = (
+                        int(start.x * w),
+                        int(start.y * h)
+                    )
+
+                    end_point = (
+                        int(end.x * w),
+                        int(end.y * h)
+                    )
+
+                    cv2.line(
+                        frame,
+                        start_point,
+                        end_point,
+                        (0, 255, 0),
+                        2
+                    )
+
         cv2.imshow("Hand Gesture Recognition", frame)
         
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    
+    detector.close()
     camera.release()
     cv2.destroyAllWindows()
 
---
+++ detection/hand_detector.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+print("Current folder:", os.getcwd())
+print("Model exists:", os.path.exists("models/hand_landmarker.task"))
+class HandDetector:
+    def __init__(self):
+
+        with open("models/hand_landmarker.task", "rb") as f:
+            model_data = f.read()
+
+        base_options = python.BaseOptions(
+            model_asset_buffer=model_data
+        )
+
+        options = vision.HandLandmarkerOptions(
+            base_options=base_options,
+            num_hands=1
+        )
+
+        self.hands = vision.HandLandmarker.create_from_options(options)
+        
+    
+    def detect(self, frame):
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        mp_image = mp.Image(
+            image_format = mp.ImageFormat.SRGB,
+            data = rgb_frame
+        )
+        results = self.hands.detect(mp_image)
+        return results
+
+    def close(self):
+        self.hands.close()
+        
